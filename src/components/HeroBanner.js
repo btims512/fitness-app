6d052411271dfe3136222aaaab8dc427d4c5b07e
Ben <71402472+btims512@users.noreply.github.com>
2023-02-07 09:53:57 -0700

    fixed widths

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,57 +9,75 @@ const HeroBanner = () => (
     position="relative"
     p="20px"
   >
-    <Typography
-      color="#FF2625"
-      fontWeight="600"
-      fontSize="36px"
-      sx={{ fontSize: { lg: "54px", xs: "36px" } }}
+    <Box
+      className="flex"
+      maxWidth="36vw"
+      display="flex"
+      flexDirection="column"
+      flexWrap="wrap"
     >
-      David's WorkoutDB
-    </Typography>
-    <Typography
-      fontWeight={700}
-      sx={{ fontSize: { lg: "44px", xs: "32px" } }}
-      mb="23px"
-      mt="30px"
-    >
-      Sweat. Smile. <br />
-      Repeat.
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px" className="p-tag">
-      Check out the most effective exercises personalized just for you.
-    </Typography>
-    <Stack>
-      <a
-        href="#exercises"
-        style={{
-          marginTop: "45px",
-          textDecoration: "none",
-          width: "200px",
-          textAlign: "center",
-          background: "#FF2625",
-          padding: "14px",
-          fontSize: "22px",
-          textTransform: "none",
-          color: "white",
-          borderRadius: "4px",
+      <Typography
+        color="#FF2625"
+        fontWeight="600"
+        fontSize="36px"
+        sx={{ fontSize: { lg: "54px", xs: "36px" } }}
+      >
+        David's WorkoutDB
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "32px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat. Smile. <br />
+        Repeat.
+      </Typography>
+      <Typography
+        fontSize="22px"
+        fontFamily="Alegreya"
+        lineHeight="35px"
+        className="p-tag"
+      >
+        Check out the most effective exercises personalized just for you.
+      </Typography>
+      <Stack>
+        <a
+          href="#exercises"
+          style={{
+            marginTop: "45px",
+            textDecoration: "none",
+            width: "200px",
+            textAlign: "center",
+            background: "#FF2625",
+            padding: "14px",
+            fontSize: "22px",
+            textTransform: "none",
+            color: "white",
+            borderRadius: "4px",
+          }}
+        >
+          Explore Exercises
+        </a>
+      </Stack>
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        sx={{
+          opacity: "0.1",
+          display: { lg: "block", xs: "none" },
+          fontSize: "130px",
         }}
       >
-        Explore Exercises
-      </a>
-    </Stack>
-    <Typography
-      fontWeight={600}
-      color="#FF2625"
-      sx={{
-        opacity: "0.1",
-        display: { lg: "block", xs: "none" },
-        fontSize: "200px",
-      }}
-    >
-      Twerkout
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+        Let's Go!
+      </Typography>
+
+      <img
+        src={HeroBannerImage}
+        alt="hero-banner"
+        className="hero-banner-img"
+      />
+    </Box>
   </Box>
 );
 
